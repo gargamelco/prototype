376588436abb93532ca6028bf93d232360748ea7
Georgi Dinkov <georgi.dinkov@scalefocus.com>
2018-01-24 13:39:01 +0200

    Instantiated some figure examples in Main()

--- Prototype/OvalShapes/Ellipse.cs
+++ Prototype/OvalShapes/Ellipse.cs
@@ -2,7 +2,6 @@
 
 namespace Prototype
 {
-    public delegate double SumDelegate();
     public class Ellipse : IFigure
     {
         protected float _x; // The x-coordinate of the upper-left corner of the bounding rectangle that defines the ellipse.
@@ -30,26 +29,24 @@ namespace Prototype
 
         public void Draw()
         {
-            SumDelegate sum = SumBetweenPointAndAFocusAndFocalLenght;
-
             DrawEllipse(_x, _y, _width, _height);
 
-            Console.WriteLine($"A new ellipse was drawn with coodinates of an embedded rectangle - X:{_x} , Y:{_y}, Width: {_width}, Height{_height} and sum: {sum}");
+            Console.WriteLine($"A new ellipse was drawn with coodinates of an embedded rectangle - X:{_x} , Y:{_y}, Width: {_width}, Height: {_height}");
         }
 
-        private double FocalLenght()
+        private float FocalLenght()
         {
             //distance of the two foci
             double f = Math.Sqrt(Math.Pow(_width, 2) - Math.Pow(_height, 2));
-            return f;
+            return Convert.ToSingle(f);
         }
 
-        public double SumBetweenPointAndAFocusAndFocalLenght()
+        public float SumBetweenPointAndAFocusAndFocalLenght()
         {
             //by definition the lenght between a point and a focus is 2a, in our case that is _width*2
             double pointToFocusLenght = _width * 2;
             double sum = pointToFocusLenght + FocalLenght();
-            return sum;
+            return Convert.ToSingle(sum);
         }
 
         public float GetSurface()
--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -10,7 +10,25 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
-            //
+            //oval shapes
+            Ellipse ellipse = new Ellipse(3, 5, 20, 15);
+            ellipse.Draw();
+            Console.WriteLine($"ellipse's sum between a poin and the two foci is { ellipse.SumBetweenPointAndAFocusAndFocalLenght() }");
+            Console.WriteLine($"ellipse's surface area is { ellipse.GetSurface() }");
+
+            Circle circle = new Circle(2, 2, 10, 10);
+            circle.Draw();
+            Console.WriteLine($"circle's surface area is { circle.GetSurface() }");
+
+            //angular shapes
+            Parallelogram parallelogram = new Parallelogram(2, 3, 4, 5, 6, 7, 8);
+            parallelogram.Draw();
+            Console.WriteLine($"parallelogram's surface area is { parallelogram.GetSurface() } ");
+
+            Square square = new Square(0, 5, 6, 10, 8, 12);
+            square.Draw();
+            Console.WriteLine($"square's surface area is { square.GetSurface() } ");
+
         }
     }
 }
